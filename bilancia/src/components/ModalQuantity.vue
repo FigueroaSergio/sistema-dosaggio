b39a66d73391776f23c1f3d05212d77ee0d657b9
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2026-07-04 18:25:30 +0200

    FIX template

--- bilancia/src/components/ModalQuantity.vue
+++ bilancia/src/components/ModalQuantity.vue
@@ -31,42 +31,41 @@ const onConfirm = () => {
     :active="active"
     :title="t('modal.quantity.title')"
     @close-modal="$emit('close-modal')"
-    ><template>
-      <p class="text-gray-600 mb-6">
-        {{ t("modal.quantity.instruction") }}
-      </p>
-      <div class="mb-6">
-        <label
-          for="desired-quantity-input"
-          class="block text-sm font-medium text-gray-700 mb-2"
-        >
-          {{ t("modal.quantity.label") }}
-        </label>
-        <input
-          id="desired-quantity-input"
-          type="number"
-          @input="if (model && model < 1) model = 0;"
-          min="1"
-          step="0.1"
-          v-model.number="model"
-          placeholder="es. 500"
-          :class="`w-full p-3 border border-gray-300 rounded-lg focus:ring-teal-500 focus:border-teal-500 transition duration-150 ${showError && error ? 'input-error' : ''}`"
-          :aria-invalid="error"
-        />
-        <div class="error-message" v-if="error && showError">
-          {{ t("modal.quantity.invalidError") }}
-        </div>
-      </div>
-      <div
-        id="recipe-info"
-        class="bg-blue-50 p-3 rounded-lg mb-6 text-sm text-blue-900"
+  >
+    <p class="text-gray-600 mb-6">
+      {{ t("modal.quantity.instruction") }}
+    </p>
+    <div class="mb-6">
+      <label
+        for="desired-quantity-input"
+        class="block text-sm font-medium text-gray-700 mb-2"
       >
-        <strong>{{ t("modal.quantity.recipe") }}</strong> {{ name }}
-        <br />
-        <strong>{{ t("modal.quantity.originalWeight") }} </strong
-        >{{ total.toFixed(2) }} g
+        {{ t("modal.quantity.label") }}
+      </label>
+      <input
+        id="desired-quantity-input"
+        type="number"
+        @input="if (model && model < 1) model = 0;"
+        min="1"
+        step="0.1"
+        v-model.number="model"
+        placeholder="es. 500"
+        :class="`w-full p-3 border border-gray-300 rounded-lg focus:ring-teal-500 focus:border-teal-500 transition duration-150 ${showError && error ? 'input-error' : ''}`"
+        :aria-invalid="error"
+      />
+      <div class="error-message" v-if="error && showError">
+        {{ t("modal.quantity.invalidError") }}
       </div>
-    </template>
+    </div>
+    <div
+      id="recipe-info"
+      class="bg-blue-50 p-3 rounded-lg mb-6 text-sm text-blue-900"
+    >
+      <strong>{{ t("modal.quantity.recipe") }}</strong> {{ name }}
+      <br />
+      <strong>{{ t("modal.quantity.originalWeight") }} </strong
+      >{{ total.toFixed(2) }} g
+    </div>
     <template #footer>
       <BaseBtn
         id="cancel-quantity-btn"
